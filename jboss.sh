40836a98586d12e96129b7a116362453fdf21aba
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-08-22 18:17:43 -0300

    Possibilita alterar BIN_DIR ao chamar funções de jboss.sh

--- jboss.sh
+++ jboss.sh
@@ -4,7 +4,7 @@ gerar="$JBOSS_INSTALA_DIR"/gerar
 
 instala_jboss() {
     echo -e "\nInstalando JBoss BPM Suite + Keycloak através do script \"$gerar\"\n"
-    BIN_DIR="$INSTALADORES_DIR" JBOSS_DESENVOLVIMENTO_DIR="$FERRAMENTAS_DIR" "$gerar" -a desenvolvimento
+    BIN_DIR="${BIN_DIR:-$INSTALADORES_DIR}" JBOSS_DESENVOLVIMENTO_DIR="$FERRAMENTAS_DIR" "$gerar" -a desenvolvimento
     
     echo -e "\nRecriando o link $JBOSS_LINK para $JBOSS_DIR ..."
     [ -L $JBOSS_LINK ] && unlink $JBOSS_LINK
@@ -15,5 +15,5 @@ remove_jboss() {
     remove_aplicacao
 
     echo -e "\nRemovendo arquivos gerados pelo uso do script \"$gerar\"\n"
-    BIN_DIR="$INSTALADORES_DIR" JBOSS_DESENVOLVIMENTO_DIR="$FERRAMENTAS_DIR" "$gerar" -a desenvolvimento -r -s
+    BIN_DIR="${BIN_DIR:-$INSTALADORES_DIR}" JBOSS_DESENVOLVIMENTO_DIR="$FERRAMENTAS_DIR" "$gerar" -a desenvolvimento -r -s
 }
