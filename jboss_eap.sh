a61b06012216eac97b62a3a1e3cf3411d63e5158
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-09-15 17:48:35 -0300

    Atualização para acompanhar a evolução do projeto javaee-ambiente

--- jboss_eap.sh
+++ jboss_eap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-gerar="$JBOSS_INSTALA_DIR"/gerar
+gerar="$JBOSS_EAP_INSTALA_DIR"/gerar
 
-instala_jboss() {
+instala_jboss_eap() {
     echo -e "\nInstalando JBoss BPM Suite + Keycloak através do script \"$gerar\"\n"
     BIN_DIR="${BIN_DIR:-$INSTALADORES_DIR}" JBOSS_DESENVOLVIMENTO_DIR="$FERRAMENTAS_DIR" "$gerar" -a desenvolvimento
     
-    echo -e "\nRecriando o link $JBOSS_LINK para $JBOSS_DIR ..."
-    [ -L $JBOSS_LINK ] && unlink $JBOSS_LINK
-    ln -s $JBOSS_DIR $JBOSS_LINK
+    echo -e "\nRecriando o link $JBOSS_EAP_LINK para $JBOSS_EAP_DIR ..."
+    [ -L $JBOSS_EAP_LINK ] && unlink $JBOSS_EAP_LINK
+    ln -s $JBOSS_EAP_DIR $JBOSS_EAP_LINK
 }
 
-remove_jboss() {
+remove_jboss_eap() {
     remove_aplicacao
 
     echo -e "\nRemovendo arquivos gerados pelo uso do script \"$gerar\"\n"
